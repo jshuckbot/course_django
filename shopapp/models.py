a8620b96df587bdfd7d50a5929c7a9a856239554
jshuckbot <gorka_ivan@mail.ru>
2023-09-30 17:06:05 +0700

    create models and makemigration

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=80)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10, unique=True)
+    address = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    total = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
