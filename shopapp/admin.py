1ef45c067d29caa0bbfe41c3200e9f835cf70f78
jshuckbot <gorka_ivan@mail.ru>
2023-10-10 15:58:11 +0700

    ready lesson 5

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopapp import models as shopapp_models
+
+
+@admin.register(shopapp_models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "total", "created"]
+    list_filter = ["name", "created"]
+    list_editable = ["price"]
+
+
+@admin.register(shopapp_models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "email", "phone", "address"]
+    list_filter = ["first_name", "created"]
+
+
+@admin.register(shopapp_models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["customer", "product", "total_price", "created"]
+    list_filter = ["total_price", "created"]
+    raw_id_fields = ["customer", "product"]
